01471de8f56caac1cb95c29e09926dbc8a4931f1
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-10-01 10:12:45 +1000

    Add basic read/write for Config and methods

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/IronWill79/blog-aggregator/internal/config"
+)
 
 func main() {
-	fmt.Println("Hey there")
+	cfg := config.Read()
+	cfg.SetUser("IronWill79")
+	cfg = config.Read()
+	fmt.Println("Config file contents:")
+	fmt.Printf("DB URL: %s\n", cfg.DBURL)
+	fmt.Printf("Username: %s\n", cfg.Username)
 }
