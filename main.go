4d7004ae9b543a7deb5d33aad3b3b52648c01d96
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-10-01 10:43:53 +1000

    Add register command and handler

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/IronWill79/blog-aggregator/internal/config"
+	"github.com/IronWill79/blog-aggregator/internal/database"
+	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 )
 
 type state struct {
 	cfg *config.Config
+	db  *database.Queries
 }
 
 type command struct {
@@ -45,18 +51,46 @@ func handlerLogin(s *state, cmd command) error {
 	return nil
 }
 
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.arguments) != 1 {
+		return errors.New("username required")
+	}
+	name := cmd.arguments[0]
+	u, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+	})
+	if err != nil {
+		return err
+	}
+	err = s.cfg.SetUser(u.Name)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("User set to %s\n", name)
+	return nil
+}
+
 func main() {
 	cfg := config.Read()
-	s := state{cfg: &cfg}
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		fmt.Printf("error opening DB: %s\n", err)
+	}
+	dbQueries := database.New(db)
+	s := state{cfg: &cfg, db: dbQueries}
 	cmds := commands{cmds: make(map[string]func(*state, command) error)}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 	if len(os.Args) < 2 {
 		fmt.Println("no command found")
 		os.Exit(1)
 	}
 	cmd := os.Args[1]
 	args := os.Args[2:]
-	err := cmds.run(&s, command{name: cmd, arguments: args})
+	err = cmds.run(&s, command{name: cmd, arguments: args})
 	if err != nil {
 		fmt.Printf("error: %s\n", err)
 		os.Exit(1)
